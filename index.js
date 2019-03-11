882f45da21239ea5a7e3703352369e615241222a
Tara-Gabriella Engelhardt <taraengy@gmail.com>
2019-03-11 20:07:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+// let drivers = ["Milo", "Otis", "Garfield"];
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) { 
+  drivers.push('Ralph'); }
+  
+function destructivelyPrependDriver(name) {
+  drivers.unshift('Bob'); 
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+function appendDriver(name) {
+  return [...drivers, name];
+}
+
+function prependDriver(name) {
+  return [name, ...drivers];
+}
+
+const newDrivers = [...drivers];
+
+function removeLastDriver() {
+ newDrivers.slice(-1);
+}
+
+function removeFirstDriver() {
+  
+}
+
